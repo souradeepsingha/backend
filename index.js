af07c050457cba437409a17f0d234f90e10031fa
Souradeep Singha <84996170+souradeepsingha@users.noreply.github.com>
2021-07-26 02:17:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 const http = require('http');
 const fs = require('fs');
 
-const hostname = '127.0.0.1';
+// const hostname = '127.0.0.1';
+const hostname = 'https://souradeepsingha.github.io/backend/';
+// https://souradeepsingha.github.io/backend/
 const port = 3000;
 const home = fs.readFileSync('index.html')
 const about = fs.readFileSync('./about.html')
